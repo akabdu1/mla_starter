4f4d6861e7631be878f2540e03ace86d3410eed1
Abdul Ka <akabdul@Abduls-MacBook-Pro.local>
2022-11-08 02:58:50 -0500

    Removed Duplication

--- MLA Starter/main.py
+++ MLA Starter/main.py
@@ -1,11 +1,8 @@
-import csv
 from typing import OrderedDict
 from xml.etree.ElementPath import find
 import pandas as pd
 import pprint
 from collections import OrderedDict
-
-
 
 
 # Treat the case where there exists employees who aren't in the ilc
@@ -23,48 +20,25 @@ def edge_cases(data, employees_info_data, country):
     return(new_serial_list)
 
 # Create dictionary result for edge cases (serials not in ilc)
-def edge_case_dictionary(serial_list, employees_info_data):
+def edge_case_dictionary(serial_list, employees_info_data, days):
     dict={}
     for serial in serial_list:
         edge_dict={}
         edge_dict['ibm_serial']= serial
-
         employee_data= employees_info_data.loc[employees_info_data['ibm_serial']==serial]
         first_name= employee_data.iloc[0]['first_name']
         last_name= employee_data.iloc[0]['last_name']
         full_name= (first_name+last_name)
         edge_dict['name']= full_name
-
-        edge_dict['claimed_hours']={
-            'sat':{
+        
+        # Create dictionary including the hours and hours missing data for each day
+        inner_dict={}
+        for day in days:
+            inner_dict[day]={
                 'hours': 0.0,
                 'is-missing': True
-            },
-            'sun':{
-                'hours': 0.0,
-                'is-missing': True
-            },
-            'mon':{
-                'hours': 0.0,
-                'is-missing': True
-            },
-            'tue':{
-                'hours': 0.0,
-                'is-missing': True
-            },
-            'wed':{
-                'hours': 0.0,
-                'is-missing': True
-            },
-            'thu':{
-                'hours': 0.0,
-                'is-missing': True
-            },
-            'fri':{
-                'hours': 0.0,
-                'is-missing': True
-            }
-        }
+                }
+        edge_dict['claimed_hours']= inner_dict
         dict[serial]=edge_dict
     return(dict, serial_list)
 
@@ -78,25 +52,18 @@ def filter_ilc(country, week, data):
 
 # Create a dataframe for each serial number 
 # Then create a dictionary of total hours for the multiple entries of each serial number
-def condense_hours(new_ilc_data):
+def condense_hours(new_ilc_data, days):
     serial_list= new_ilc_data['ibm_serial'].tolist()
     serial_list = list(dict.fromkeys(serial_list))
     full_hours_dict= {}
 
-    for i in range(len(serial_list)):
-        # filter data frame by serial number 
-        x= serial_list[i]
+    for x in serial_list:
+        # filter data frame by serial number
+        dic={}
         new_data= new_ilc_data.loc[(new_ilc_data['ibm_serial']== x)]
-
-        sat_total = new_data.loc[:, 'sat'].sum()
-        sun_total = new_data.loc[:, 'sun'].sum()
-        mon_total = new_data.loc[:, 'mon'].sum()
-        tue_total = new_data.loc[:, 'tue'].sum()
-        wed_total = new_data.loc[:, 'wed'].sum()
-        thu_total = new_data.loc[:, 'thu'].sum()
-        fri_total = new_data.loc[:, 'fri'].sum()
-
-        dic= {'sat_total': sat_total, 'sun_total': sun_total, 'mon_total': mon_total, 'tue_total': tue_total, 'wed_total': wed_total, 'thu_total': thu_total, 'fri_total': fri_total,}
+        for day in days:
+            total= new_data.loc[:, day].sum()
+            dic[f'{day}_total']= total
         full_hours_dict[x]= dic
     return(full_hours_dict)
 #condense_hours()
@@ -108,8 +75,11 @@ def find_missing_hours(full_hours_dict):
     hours_dic={}
     for index in full_hours_dict: 
         hours=full_hours_dict[index]
-        if hours['mon_total']==0.0 or hours['tue_total']==0.0 or hours['wed_total'] ==0.0 or hours['thu_total'] ==0.0 or hours['fri_total'] == 0.0:
-            serial_list.append(index)
+        days_index=['mon_total', 'tue_total', 'wed_total', 'thu_total', 'fri_total']
+        for day_index in days_index:
+            if hours[day_index] == 0.0:
+                serial_list.append(index)
+                break    
     for i in serial_list:
         hours_dic[i]= full_hours_dict[i]
     return serial_list, hours_dic
@@ -118,60 +88,24 @@ def find_missing_hours(full_hours_dict):
 
 # Create Claimed Hours Dictionary
 # Make key the serial Number
-def claimed_hours(serial_list, hours_dic):
+def claimed_hours(serial_list, hours_dic, days):
     claimed_hours_dict={}
+    week_dict={}
+
+    #Find and create dictionary of claimed hours components for each day
+    n_dict={}
     for i in serial_list:
-        sat_hours= hours_dic[i]['sat_total']
-        if sat_hours ==0.0:
-            sat_is_missing= True
-        else:
-            sat_is_missing= False
+        for day in days:
+            hours = hours_dic[i][f'{day}_total']
+            is_missing= False
+            if hours== 0.0:
+                is_missing= True
+            n_dict[day]={
+                'hours': hours,
+                'is-missing': is_missing
+            }
+            week_dict[day]= n_dict[day]
 
-        sun_hours= hours_dic[i]['sun_total']
-        if sun_hours ==0.0:
-            sun_is_missing= True
-        else:
-            sun_is_missing= False
-        
-        mon_hours= hours_dic[i]['mon_total']
-        if mon_hours ==0.0:
-            mon_is_missing= True
-        else:
-            mon_is_missing= False
-
-        tue_hours= hours_dic[i]['tue_total']
-        if tue_hours ==0.0:
-            tue_is_missing= True
-        else:
-            tue_is_missing= False
-        
-        wed_hours= hours_dic[i]['wed_total']
-        if wed_hours ==0.0:
-            wed_is_missing= True
-        else:
-            wed_is_missing= False
-        
-        thu_hours= hours_dic[i]['thu_total']
-        if thu_hours ==0.0:
-            thu_is_missing= True
-        else:
-            thu_is_missing= False
-
-        fri_hours= hours_dic[i]['fri_total']
-        if fri_hours ==0.0:
-            fri_is_missing= True
-        else:
-            fri_is_missing= False
-        
-        week_dict={ 
-            'sat':{'hours':sat_hours, 'is-missing':sat_is_missing},
-            'sun':{'hours':sun_hours, 'is-missing':sun_is_missing},
-            'mon':{'hours':mon_hours, 'is-missing':mon_is_missing},
-            'tue':{'hours':tue_hours, 'is-missing':tue_is_missing},
-            'wed':{'hours':wed_hours, 'is-missing':wed_is_missing},
-            'thu':{'hours':thu_hours, 'is-missing':thu_is_missing},
-            'fri':{'hours':fri_hours, 'is-missing':fri_is_missing}
-        }
         claimed_hours_dict[i]={}
         claimed_hours_dict[i]['claimed-hours']= week_dict
     return(claimed_hours_dict)
@@ -211,13 +145,15 @@ def main():
     employees_info_data= pd.read_excel('employees.xlsx')
     country= 897
     week='2022-09-30'
+    days= ['sat', 'sun', 'mon', 'tue', 'wed', 'thu', 'fri']
+
 
     new_serial_list= edge_cases(data, employees_info_data, country)
-    edge_dict, edge_serial_list= edge_case_dictionary(new_serial_list, employees_info_data)
+    edge_dict, edge_serial_list= edge_case_dictionary(new_serial_list, employees_info_data, days)
     new_ilc_data= filter_ilc(country, week, data)
-    full_hours_dict= condense_hours(new_ilc_data)
+    full_hours_dict= condense_hours(new_ilc_data, days)
     serial_list, hours_dic=find_missing_hours(full_hours_dict )
-    claimed_hours_dict= claimed_hours(serial_list, hours_dic)
+    claimed_hours_dict= claimed_hours(serial_list, hours_dic, days)
     names_dict= names(serial_list, employees_info_data)
 
     final_dataframe(serial_list, edge_dict, edge_serial_list, names_dict, claimed_hours_dict)
